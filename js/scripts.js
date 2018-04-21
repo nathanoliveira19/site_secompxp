28ef45461021e2983c44c8e63edb9004464adfd1
igor <igraphmagollo@gmail.com>
2018-04-21 12:22:15 -0300

    Funções para slideshow adicionadas

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var slideIndex = 1;
+var timer;
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" ativo", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " ativo";
+  clearTimeout(timer);
+  timer = setTimeout(automatic,6000);
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function automatic(){
+  plusSlides(1);
+}
